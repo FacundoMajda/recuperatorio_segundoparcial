92aff2886aa72d5c40e70bca9d2a89d4857f640d
FacundoMajda <facundomajda13@gmail.com>
2023-07-05 15:02:58 -0300

    fix error de llamado de variables de entorno, ya conecta perfectamente la conexion a la db y se crearon las tablas

--- database.js
+++ database.js
@@ -1,18 +1,17 @@
 const { Sequelize, DataTypes } = require("sequelize");
+
 require("dotenv").config();
-// conexion a la base de datos
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect:
-      process.env
-        .DB_DIALECT /*sequelize requiere especificar. se carga el dialecto, en este caso mysql */,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   }
 );
-
 const conexionDB = async () => {
   try {
     await sequelize.authenticate();
